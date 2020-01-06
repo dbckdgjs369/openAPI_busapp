384d177c65b615de393e8b1cefdbfe841ad883e8
dbckdgjs369 <dbckdgjs1130@naver.com>
2020-01-06 21:40:06 +0900

    add tab layout

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,14 +19,26 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+            TabHost tab_host = (TabHost) findViewById(R.id.tabhost);
+            tab_host.setup();
+
+            TabHost.TabSpec ts1 = tab_host.newTabSpec("tab1");
+            ts1.setIndicator("즐겨찾기");
+            ts1.setContent(R.id.tab1);
+            tab_host.addTab(ts1);
+
+            TabHost.TabSpec ts2 = tab_host.newTabSpec("tab2");
+            ts2.setIndicator("노선");
+            ts2.setContent(R.id.tab2);
+            tab_host.addTab(ts2);
+
+        TabHost.TabSpec ts3 = tab_host.newTabSpec("tab3");
+        ts3.setIndicator("정류장");
+        ts3.setContent(R.id.tab3);
+        tab_host.addTab(ts3);
+
+
+        tab_host.setCurrentTab(0);
     }
 
     @Override
